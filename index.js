676f985db03ec74c58ad9ec2db1d961680162a74
Rony Laforest <rrlaforest@gmail.com>
2023-09-20 15:01:33 +0200

    git error fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Iteration 1: Names and Input
-const hacker1 = 'Rony';
+const hacker1 = "Rony";
 console.log(`The driver's name is ${hacker1}.`);
-const hacker2 = 'Elena';
+const hacker2 = "Elena";
 console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
